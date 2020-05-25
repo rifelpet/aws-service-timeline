1bede8f4ab2cf21a68680fa91e54e36473552998
Peter Rifel <pgrifel@gmail.com>
2020-05-25 08:24:23 -0500

    remove unnecessary js callback

--- timeline.js
+++ timeline.js
@@ -11,7 +11,7 @@ function loadTimeline(cb) {
   xhttp.send();
 }
 
-function populateDropdowns(regions, services, cb) {
+function populateDropdowns(regions, services) {
   let regionSelect = document.getElementById("regions");
   regions.forEach(function (region) {
     let option = document.createElement("option");
@@ -29,7 +29,6 @@ function populateDropdowns(regions, services, cb) {
     option.appendChild(text);
     serviceSelect.appendChild(option);
   });
-  cb();
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -40,41 +39,37 @@ document.addEventListener('DOMContentLoaded', function() {
     let regions = Object.keys(regionLaunchDates).sort();
     let services = Object.keys(serviceLaunchDates).sort();
 
-    populateDropdowns(regions, services, function() {
-      let data = [];
-      regions.forEach(function (region) {
-        if (region != 'eu-west-1') {
-          return
-        }
-        let trace = {
-          //x: [20, 14, 23],
-          y: services,
-          name: region,
-          orientation: 'h',
-          marker: {
-            width: 1
-          },
-          type: 'bar'
-        };
-        let x = []
-        // regionServices = [];
-        services.forEach(function (service){
-          let startDate = serviceLaunchDates[service];
-          if (service in regionTimeline[region]) {
-            let regionDate = Date.parse(regionTimeline[region][service]["date"])
-            let delayWeeks = (regionDate - startDate) / 1000 / 60 / 60 / 24 / 7;
-            x.push(delayWeeks)
-          } else {
-            x.push(-100)
-          }
-        });
-        trace["x"] = x;
-        if (x.length > 0) {
-          data.push(trace);
+    populateDropdowns(regions, services);
+    let data = [];
+    regions.forEach(function (region) {
+      let trace = {
+        //x: [20, 14, 23],
+        y: services,
+        name: region,
+        orientation: 'h',
+        marker: {
+          width: 1
+        },
+        type: 'bar'
+      };
+      let x = []
+      // regionServices = [];
+      services.forEach(function (service){
+        let startDate = serviceLaunchDates[service];
+        if (service in regionTimeline[region]) {
+          let regionDate = Date.parse(regionTimeline[region][service]["date"])
+          let delayWeeks = (regionDate - startDate) / 1000 / 60 / 60 / 24 / 7;
+          x.push(delayWeeks)
+        } else {
+          x.push(-100)
         }
       });
+      trace["x"] = x;
+      if (x.length > 0) {
+        data.push(trace);
+      }
+    });
 
-      Plotly.newPlot('graph', data, {showSendToCloud:true});
-    })
+    Plotly.newPlot('graph', data, {showSendToCloud:true});
   })
 })
